e07b15de026171dc5bec1f8279122858df7b3b94
andres <jeypar92@gmail.com>
2017-11-09 21:32:05 -0500

    lo cambie para generar html propio 09/11/2017

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import html
-
+from html import GeneraHtml
+import cherrypy, html
 import cherrypy, os
 import os
 
@@ -7,15 +7,23 @@ import os
 class goodAndDevil(object):
     @cherrypy.expose
     def index(self):
-        return self.generaHtml()
+        return GeneraHtml().createList()
 
-    def generaHtml(self):
-        html = ""
-        for i in range(0,10):
-            html += str(i)
+    @cherrypy.expose
+    def gregorio(self):
+        return "rece gregorio"
 
-        return html
+    @cherrypy.expose
+    def andres(self, nota=0):
+        return """
+        <!DOCTYPE html><html><body><div id="demo"><h1>The XMLHttpRequest Object</h1><button type="button" onclick="loadDoc()">Change Content</button></div><script>function loadDoc(){var xhttp=new XMLHttpRequest(); xhttp.onreadystatechange=function(){if (this.readyState==4 && this.status==200){document.getElementById("demo").innerHTML=this.responseText;}}; xhttp.open("GET", "http://127.0.0.1:8000/", true); xhttp.send();}</script></body></html>
+        """
 
+    @cherrypy.expose
+    def pagina(self):
+        pagina = "<!DOCTYPE html><html><body><h1>$titulo</h1><p>My first paragraph.</p></body></html>"
+        pagina = pagina.replace("$titulo", GeneraHtml().createList())
+        return pagina
 
 
 if __name__ == '__main__':
@@ -30,6 +38,21 @@ if __name__ == '__main__':
         }
     }
     cherrypy.config.update({'server.socket_host': '127.0.0.1', })
-    cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')), })
+    cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '8000')), })
     cherrypy.quickstart(goodAndDevil(), '/', conf)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
